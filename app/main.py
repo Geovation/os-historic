47e7b5dc527fd4f482ce700035917815c0f5e425
andreas.mitrou@geovation.uk <andreas.mitrou@geovation.uk>
2020-05-18 10:49:13 +0100

    fix-main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 import os
+import json
 from utils.utilities import Utilities
+from db.Postgres import PgDatabaseManager, QueryResultType
+from spatial.toolbox import SpatialDb, OSMMTopoUtilites, DeltaStrategy
+from trackers.osmm import TopographyLayerTrackerDb
 
 def ImportDB(workspace):
 
@@ -33,4 +37,25 @@ def ImportDB(workspace):
             if (dump_file != None) and (db_name != None):
                 Utilities.ExecuteShellScript(importScript,server,port,user,db_name,dump_file)
 
-ImportDB("/Users/amitrou/Documents/CodeProjects/historical_data/host_data")
+def DbConnect():
+    pgManager = PgDatabaseManager("localhost","postgres","andreas","osmm_2018",7432)
+    results = pgManager.ExecuteQuery("SELECT NOW()")
+    print(results)
+
+def ReadConfig(path):
+    with open(path) as f:
+        data = json.load(f)
+    return data
+
+def GenerateResultsView(featureName, configCurrentDb):
+    currentVersionDB = PgDatabaseManager(configCurrentDb["host"],configCurrentDb["username"],configCurrentDb["password"],configCurrentDb["db_name"],configCurrentDb["port"])
+    query = "select a.*, b.version_first_appearance from osmm_topo.clipped_topo_area a LEFT JOIN public.version_feature_map b ON a.toid = b.toid  where ARRAY_TO_STRING(a.descriptivegroup,',') like '%" + featureName +"%'"
+    currentVersionDB.ExecuteQueryAsTableOrView(query,"V_VERSION_IDENTIFIED_" + featureName,"view")
+
+if __name__ == "__main__":
+    config = ReadConfig(os.path.join(os.getcwd(),"app","db_compare_settings.json"))
+    featureName = "Building"
+    attributeQuery = "ARRAY_TO_STRING(descriptivegroup,',') like '%" + featureName + "%'"
+    tracker = TopographyLayerTrackerDb(config)
+    labels = tracker.SetVersionLabel(attributeQuery,"toid",[531195.2968,170817.8032,536873.2005,180696.8954], True)
+    print(labels)
