bd6d18f9c310f5e2916228b94dfb16a4072e21f7
andreas.mitrou@geovation.uk <andreas.mitrou@geovation.uk>
2020-05-18 10:29:56 +0100

    fix-restore-db.sh

--- restore-db.sh
+++ restore-db.sh
@@ -3,5 +3,6 @@ echo "Creating Database $1"
 #1 = Datatabase name
 #2 = dumb filename
 psql -h $1 -p $2 -U $3 -c "CREATE DATABASE $4"
-psql -h $1 -p $2 -U $3 -c "CREATE EXTENSION IF NOT EXISTS postgis"
-#pg_restore -h localhost -U postgres -d $1  -j 8 -v "/opt/input_data/OSMM_TOPO_January18.dump.gz"
+psql -h $1 -p $2 -U $3 -d $4 -c "CREATE EXTENSION IF NOT EXISTS postgis"
+#pg_restore -h $1 -p $2 -U $3s -d $4  -j 8 -v "/opt/input_data/OSMM_TOPO_January18.dump.gz"
+pg_restore -h $1 -p $2 -U $3 -d $4 -v "$PWD/host_data/$5"
